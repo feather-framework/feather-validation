39bc7d3c945e1ec5fc8a781562c4532e4cc9e35b
László Kovács <atomantinator@gmail.com>
2024-05-27 10:15:37 +0200

    fix public ValidatorError init

--- Sources/FeatherValidation/Validators/ValidatorError.swift
+++ Sources/FeatherValidation/Validators/ValidatorError.swift
@@ -1,6 +1,10 @@
 /// Validator error object
 public struct ValidatorError: Error {
 
+    public init(failures: [Failure]) {
+        self.failures = failures
+    }
+
     /// List of failure objects
     public let failures: [Failure]
 }
